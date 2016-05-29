96e0aa18b3afeae1f7ace7f1966abb0e282c13fe
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-05-29 22:19:00 +0200

    readme and opt.

--- src/Core.php
+++ src/Core.php
@@ -6,25 +6,49 @@ namespace AgencyBoilerplate\Handlebars;
 class Core
 {
 
+  protected static $instance = null;
+
   /**
    * @var \Handlebars\Handlebars
    */
-  private $engine;
+  protected $engine;
 
-  public function __construct($partialBaseDir, $options = array())
+  public static function getInstance()
+  {
+    self::hasInstance();
+    return self::$instance;
+  }
+
+  public function __construct($engine)
+  {
+    $this->engine = $engine;
+  }
+
+  public static function init($options)
   {
     if (is_array($options)) {
       $options = array_merge([
+        'partialDir' => '.',
         'extension' => '.hbs',
         'prefix' => ''
       ], $options);
     }
-    $baseDir = $partialBaseDir;
-    $this->engine = new \Handlebars\Handlebars(array(
+    $baseDir = $options['partialDir'];
+    self::$instance = new self(new \Handlebars\Handlebars(array(
       'helpers' => new Helpers(),
       'loader' => new \AgencyBoilerplate\Handlebars\Loader\FilesystemLoader($baseDir, $options),
       'partials_loader' => new \AgencyBoilerplate\Handlebars\Loader\FilesystemLoader($baseDir, $options)
-    ));
+    )));
+    return self::$instance;
+  }
+
+  public static function hasInstance()
+  {
+    if (!self::$instance) {
+      throw new \InvalidArgumentException(
+        'init core and get instance from core.'
+      );
+    }
   }
 
   /**
@@ -32,6 +56,7 @@ class Core
    */
   public function getEngine()
   {
+    self::hasInstance();
     return $this->engine;
   }
 
--- tests/stringify/index.php
+++ tests/stringify/index.php
@@ -1,7 +1,9 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
-$core = new \AgencyBoilerplate\Handlebars\Core(__DIR__ . '/tmpl/');
+$core = \AgencyBoilerplate\Handlebars\Core::init([
+  'partialDir' => __DIR__ . '/tmpl/'
+]);
 
 $data = [
   'object' => [
